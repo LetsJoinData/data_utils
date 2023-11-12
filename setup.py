9429ed0425ff01082e6848b24adcd2ea7062a501
leonDataCraftman <leon@letsjoindata.com>
2023-11-12 12:51:51 -0600

    adding setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='data_utils',
+      version='0.1',
+      description='Base Python Utils for moving data to cloud databases',
+      url='https://github.com/LetsJoinData/data_utils',
+      author='Lets Join Data',
+      author_email='leon@letsjoindata.com',
+      license='MIT',
+      packages=['data_utils'],
+      install_requires=[
+          'pandas',
+          'snowflake-connector-python',
+          'sqlalchemy',
+          'snowflake-connector-python[pandas]',
+          'snowflake-sqlalchemy',
+          'pysftp',
+          'retrying'
+      ],
+      zip_safe=False)
