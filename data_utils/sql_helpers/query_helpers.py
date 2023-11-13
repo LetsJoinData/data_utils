ca6ea22f7c103874fba4eb9d3745cadfede888d2
leonDataCraftman <leon@letsjoindata.com>
2023-11-12 23:41:55 -0600

    adding new feature

--- data_utils/sql_helpers/query_helpers.py
+++ data_utils/sql_helpers/query_helpers.py
@@ -74,3 +74,5 @@ def create_stage(cursor, schema, stage_name=None, temporary_stage=False, format_
     else:
         sql = CREATE_STAGE.format(params=format_params, schema=schema, stage_name=stage_name)
     execute_sql(cursor, sql)
+
+    return stage_name
